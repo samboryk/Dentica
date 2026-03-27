82f9707a96cfc467e47bdda0bf6f8d46aeface11
samboryk <dmytrosamboryk@gmail.com>
2026-03-27 23:36:47 +0200

    шось добавив

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,25 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+// 1. Сюди треба буде вставити ваш публічний токен
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtYm9yeWsiLCJhIjoiY21uMmM4dDF2MHphYjJwcXNranh4M2ZwbyJ9.guETzstSNyXsukmzNScjMg';
+
+// 2. Ініціалізуємо карту
+const map = new mapboxgl.Map({
+    container: 'map', // ID нашого div
+    style: 'mapbox://styles/mapbox/dark-v11', // Розкішна темна тема Mapbox
+    center: [30.5332, 50.4265], // Координати: [Довгота, Широта]
+    zoom: 15.5 // Рівень наближення
+});
+
+// 3. Додаємо маркер, колір якого збігається з вашою кнопкою (#BFDCFF)
+const marker = new mapboxgl.Marker({ color: '#BFDCFF' })
+    .setLngLat([30.5332, 50.4265])
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // Спливаюче вікно при кліку
+            .setHTML('<h3 style="color:#000; margin:0; font-family:sans-serif;">Dentica</h3><p style="color:#333; margin:5px 0 0; font-family:sans-serif;">вул. Євгена Коновальця, 36Е</p>')
+    )
+    .addTo(map);
+
+// Вимикаємо зум скролом миші, щоб сторінка не "застрягала" при прокручуванні вниз
+map.scrollZoom.disable();
